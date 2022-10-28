8580931323817032d9501b6a6ae163fb75e3ea91
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-28 15:53:20 +0200

    acabado sarrus

--- sarrus.py
+++ sarrus.py
@@ -18,6 +18,16 @@ def mostrar_matriz(matriz):
             print(f"{matriz[i][j]} ", end="")
         print()
         
+def determinantesarrus(matriz):
+    determinante = 0
+    for i in range(3):
+        determinante += matriz[0][i] * matriz[1][(i+1)%3] * matriz[2][(i+2)%3]
+        determinante -= matriz[0][i] * matriz[1][(i+2)%3] * matriz[2][(i+1)%3]
+    return determinante
+
+
 matriz = matriz()
 matriz = rellenar_matriz(matriz)
 mostrar_matriz(matriz)
+determinante = determinantesarrus(matriz)
+print(f"el determinante es {determinante}")
