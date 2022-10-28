9c23adb8ee7af4b264e99ffd6cbc66d742a534a9
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-28 15:38:03 +0200

    añado las funciones que ordenan

--- naves.py
+++ naves.py
@@ -3,3 +3,20 @@ import csv
 with open('naves.csv', 'r') as file:
     reader1 = csv.DictReader(file, delimiter=';')
     star_naves = list(reader1)
+
+def ordenar_naves():
+    naves = []
+    for row in star_naves:
+        naves.append(row['Name'])
+    naves.sort()
+    print(f"\n\nLas naves ordenadas por nombre son {naves}")
+
+def ordenar_largo():
+    naves_largo = []
+    for row in star_naves:
+        naves_largo.append(row['Name'] + " " + row['Length'])
+    naves_largo.sort() ; naves_largo.reverse()
+    print(f"\n\nLas naves ordenadas por largo son {naves_largo}")
+
+ordenar_largo()
+ordenar_naves()
